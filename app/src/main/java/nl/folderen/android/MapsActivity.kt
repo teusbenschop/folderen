66e2914655d153b6afe11dc54e02df39e5ee32b1
Teus Benschop <teusjannette@gmail.com>
2019-07-11 18:07:40 +0200

    Handle permissions

--- app/src/main/java/nl/folderen/android/MapsActivity.kt
+++ app/src/main/java/nl/folderen/android/MapsActivity.kt
@@ -1,7 +1,15 @@
 package nl.folderen.android
 
+import android.content.DialogInterface
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,15 +19,21 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
+
 
 // Based on this tutorial:
 // https://www.raywenderlich.com/230-introduction-to-google-maps-api-for-android-with-kotlin#toc-anchor-001
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+class MapsActivity :
+    AppCompatActivity(),
+    OnMapReadyCallback,
     GoogleMap.OnMarkerClickListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var timer: Timer
+    private lateinit var timerTask: TimerTask
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +42,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Handle permissions before map is created.
+        checkPermissions()
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        timer = Timer()
+        // timerTask = TimerTask()
+        //timer.scheduleAtFixedRate(timerTask, 1000, 1000)
     }
 
     /**
@@ -57,12 +79,140 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
     override fun onMarkerClick(p0: Marker?) = false
-
     /*
     override fun onMarkerClick(p0: Marker?): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
     */
+
+    private fun runTimerTask() {
+        timerTask = object : TimerTask() {
+            override fun run() {
+
+                runOnUiThread { Log.d("Folderen", "Folderen") }
+
+                runOnUiThread { printLocation() }
+
+            }
+        }
+    }
+
+    private fun printLocation() {
+        Log.d ("folderen", "printLocation")
+        /*
+        var permissionsGranted = true
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsGranted = false
+        }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsGranted = false
+        }
+        if (!permissionsGranted) {
+            Log.d("Folderen", "No permissions to access device's location")
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(
+            this
+        ) { location ->
+            if (location != null) {
+                Log.d("Location", location!!.toString())
+            } else {
+                Log.d("Location", "is null")
+            }
+        }
+        */
+    }
+
+    private fun checkPermissions(): Boolean {
+        // https://developer.android.com/training/permissions/requesting
+
+        // The permissions to check for whether they have been granted.
+        val context = applicationContext
+        lateinit var info: PackageInfo
+        try {
+            info = packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
+        } catch (e: Exception) {
+            // There's no package info.
+            // This implies there's no permissions to request for.
+            // So: Done.
+            return true
+        }
+
+        val permissionsToCheck = info!!.requestedPermissions
+
+        // Check if the permissions have been granted.
+        // Collect the permissions to ask the user to grant.
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in permissionsToCheck) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission)
+            }
+        }
+
+        // If all permissions have been granted by the user already: Done.
+        if (permissionsToRequest.isEmpty()) {
+            return true
+        }
+
+        // One or more permissions are not yet granted: Request them.
+        val permissionsToRequestArray = permissionsToRequest.toArray(arrayOfNulls<String>(permissionsToRequest.size))
+        ActivityCompat.requestPermissions(this, permissionsToRequestArray, 0)
+
+        // Indicate to the caller that one or more permissions have not yet been granted.
+        return false
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        Log.d("onRequestPermissions", requestCode.toString())
+
+        // The permissions to check for whether they have been granted by the user.
+        val context = applicationContext
+        lateinit var info: PackageInfo
+        try {
+            info = packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
+        } catch (e: Exception) {
+            // There's no package info.
+            // This implies there's no permissions to check for.
+            // So: Done.
+            return
+        }
+
+        val permissionsToCheck = info!!.requestedPermissions
+
+        // Check if the permissions have been granted.
+        var alertUser = false
+        for (permission in permissionsToCheck) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                alertUser = true
+            }
+        }
+
+        // If not all permissions have been granted, alert the user.
+        if (alertUser) {
+            AlertDialog.Builder(this)
+                .setTitle("Permissions needed")
+                .setMessage("Please grant all permissions for the app to work as designed")
+                .setPositiveButton("Ok", DialogInterface.OnClickListener { dialogInterface, i ->
+                    val toast = Toast.makeText(
+                        applicationContext,
+                        "The app is running with reduced functionality",
+                        Toast.LENGTH_LONG
+                    )
+                    toast.show()
+                })
+                .create()
+                .show()
+        }
+    }
 
 }
 
